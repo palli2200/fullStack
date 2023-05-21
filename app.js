fdd7d1500bb852a67a2cd28ab41cd6c58619d630
PalliNaganjaneyulu <81254851+PalliNaganjaneyulu@users.noreply.github.com>
2023-05-21 15:47:03 +0530

    morgon and ejs introduction

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ let express = require('express')
 let app = express();
 let dotenv = require('dotenv');
 dotenv.config()
+let morgan = require('morgan')
+let fs = require('fs')
 let port = process.env.port || 6700;
 // let port =6700;
 
 let categoryRouter = require('./src/controller/categoryRouter');
 let productRouter = require('./src/controller/productRouter');
 
+// middleWear
+// app.use(morgan('dev'))
+// app.use(morgan('common'))
+app.use(morgan('common',{stream:fs.createWriteStream('./app.log')}))
 
+// static file path 
+app.use(express.static(__dirname+'/public'))
+
+// html file path 
+app.set('views','./src/views')
+
+// view engine 
+app.set('view engine','ejs')
 
 // default route
 app.get('/',(req,res)=>{
-    res.send('hi from express');
+    // res.send('hi from express');
+    res.render('index')
 })
 
 app.use('/category', categoryRouter)
